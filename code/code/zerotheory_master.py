92dced99f49c31b7632cb62a4cf285844f014d87
Manuel <andres17032017@gmail.com>
2025-10-31 17:17:28 +0100

    Create zerotheory_master.py

---
+++ code/code/zerotheory_master.py
@@ -0,0 +1,11 @@
+from FundamentalConstants import FundamentalConstants as FC
+
+def main():
+    fc = FC()
+    print("THE ZERO THEORY - PREDICTIONS")
+    print(f"n_s = {fc.n_s}")
+    print(f"Lambda = {fc.Lambda}")
+    print(f"M_GUT = {fc.M_GUT} GeV")
+
+if __name__ == "__main__":
+    main()
